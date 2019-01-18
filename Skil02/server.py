0e53ffc54bcab064b643b0941a56f38fab1184d9
Kalliarolunni <gustigaur@gmail.com>
2019-01-18 12:18:06 +0000

    bunir?

---
+++ Skil02/server.py
@@ -0,0 +1,40 @@
+import pickle
+import socket
+from os import listdir
+from os.path import isfile, join
+from re import *
+
+HOST = socket.gethostname()
+PORT = 12345
+
+s = socket.socket()
+s.bind((HOST,PORT))
+s.listen(2)
+
+
+onlyfiles = [f for f in listdir("./") if (isfile(join("./", f)) and match(".*.txt", f))]
+while True:
+    data = pickle.dumps(onlyfiles)
+    c, addr = s.accept()
+    val = pickle.loads(c.recv(1024))
+    if val == 1:
+        c.send(data)
+        recieve=c.recv(1024)
+        try:
+            svar = pickle.loads(recieve)
+            print(svar)
+            uppsk=onlyfiles[svar-1]
+            f = open(uppsk,'rb')
+            print('Connected by', addr)
+            data = f.read(1024)
+            c.send(data)
+            f.close()
+        except:
+            pass
+    elif val == 2:
+        file = pickle.loads(c.recv(1024))
+        f = open(file ,'wb')
+        data = c.recv(1024)
+        f.write(data)
+        f.close()
+
