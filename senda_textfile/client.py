56447554e6d40a48227386bcc0efb6a85b56384a
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2019-01-18 11:17:46 +0000

    Add files via upload

---
+++ senda_textfile/client.py
@@ -0,0 +1,20 @@
+import socket
+import pickle
+
+#HOST = socket.gethostname()
+HOST = '10.220.226.55'
+PORT = 12345
+
+
+s = socket.socket()
+s.connect((HOST,PORT))
+
+list = pickle.loads(s.recv(1024))
+for x in range(len(list)):
+    print(x+1, list[x])
+numer = 1
+s.send(pickle.dumps(numer))
+f = open(list[numer-1],'wb')
+data = s.recv(1024)
+f.write(data)
+f.close()
