a627aab8c4a9c24eb27a48d8970f52e0449f3edd
Kalliarolunni <gustigaur@gmail.com>
2019-01-18 11:17:34 +0000

    verkefni 1 búið

---
+++ senda_textfile/server.py
@@ -0,0 +1,34 @@
+import pickle
+import socket
+from os import listdir
+from os.path import isfile, join
+from re import *
+
+HOST = socket.gethostname()
+PORT = 12345
+
+s = socket.socket()
+s.bind((HOST,PORT))
+s.listen(2)
+
+
+onlyfiles = [f for f in listdir("./") if (isfile(join("./", f)) and match(".*.txt", f))]
+while True:
+    data = pickle.dumps(onlyfiles)
+    c, addr = s.accept()
+    c.send(data)
+    recieve=c.recv(1024)
+    print("jhasdjksda",recieve)
+    try:
+        svar = pickle.loads(recieve)
+        print(svar)
+        uppsk=onlyfiles[svar-1]
+        f = open(uppsk,'rb')
+        print('Connected by', addr)
+        data = f.read(1024)
+        print("asdasd", data)
+        c.send(data)
+        f.close()
+    except:
+        pass
+
