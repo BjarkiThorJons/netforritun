22889f3340cdb2e873ff80adc0a33fb128601284
KolbeinnIn <kolbeinnin264@nemi.tskoli.is>
2019-01-18 11:36:10 +0000

    Add files via upload

--- senda_textfile/client.py
+++ senda_textfile/client.py
@@ -1,20 +1,22 @@
+#Kolbeinn, Ágúst, Bjarki
+#Skilaverkefni 1
 import socket
 import pickle
 
-#HOST = socket.gethostname()
 HOST = '10.220.226.55'
 PORT = 12345
 
-
 s = socket.socket()
-s.connect((HOST,PORT))
+s.connect((HOST, PORT))
+data = pickle.loads(s.recv(1024))
+for x in range(len(data)):
+    print(x+1, data[x])
 
-list = pickle.loads(s.recv(1024))
-for x in range(len(list)):
-    print(x+1, list[x])
-numer = int(input("Veldu uppskrift með tölu: "))
+
+numer = int(input("Veldu númer: "))
 s.send(pickle.dumps(numer))
-f = open(list[numer-1],'wb')
+f = open(data[numer-1], 'wb')
+
 data = s.recv(1024)
 f.write(data)
 f.close()
