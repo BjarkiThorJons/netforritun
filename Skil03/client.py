e7b0d195aed22177abb03cc2b1d18f946c2e87ef
KolbeinnIn <kolbeinnin264@nemi.tskoli.is>
2019-01-30 14:45:38 +0000

    skil03 búið

--- Skil03/client.py
+++ Skil03/client.py
@@ -6,22 +6,38 @@ import os
 
 HOST = '10.220.226.55'
 PORT = 12345
-
-while True:
-    s = socket.socket()
-    s.connect((HOST, PORT))
-    tries = 5
+stafalisti = []
+s = socket.socket()
+s.connect((HOST, PORT))
+tries = 5
+while tries > 0:
     stafur = ""
-    while tries > 0:
-        strengur1 = s.recv(1024)
-        print("Þú hefur %s líf eftir" % tries)
-        print(strengur1.decode())
-        while len(stafur) == 1:
-            stafur = input("Sláðu inn staf: ").encode()
-        s.send(stafur)
-        lif = s.recv(1024).decode()
-        tries = int(lif)
-    s.close()
+    strengur1 = s.recv(1024).decode()
+    print("Þú hefur %s líf eftir" % tries)
+    print("Þú ert búin/n að prufa:")
+    for x in stafalisti:
+        print(x, end=" ")
+    print()
+    print(strengur1)
 
+    while True:
+        stafur = input("Sláðu inn staf: ")
+        if len(stafur) == 1:
+            break
+        else:
+            print("aðeins einn stafur!!!")
+    stafalisti.append(stafur)
+    s.send(pickle.dumps(stafur))
+    lif = s.recv(1024).decode()
+    tries = int(lif)
+
+    if tries == 6:
+        print("Til hamingju, þú vannst!")
+        print("Oðrið var: %s" % s.recv(1024).decode().replace(" ", ""))
+        break
+if tries == 0:
+    print("Til hamingju, þú fokking tapaðir")
+    print("Oðrið var: '%s'" % s.recv(1024).decode().replace(" ", ""))
+s.close()
 
 
